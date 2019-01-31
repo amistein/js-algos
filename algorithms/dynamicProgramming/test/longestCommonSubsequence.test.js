df1b3c8cd3593dc90c6422a6002af4cbe2e42313
Amrom Steinmetz <amroms@gmail.com>
2019-01-30 21:51:48 -0500

    lcs: debug

--- algorithms/dynamicProgramming/test/longestCommonSubsequence.test.js
+++ algorithms/dynamicProgramming/test/longestCommonSubsequence.test.js
@@ -1,7 +1,7 @@
 import lcs from '../src/longestCommonSubsequence'
 
 describe('find the longest common subsequence of two string', () => {
-	test('returns the length of the lcs', () => {
-		expect(lcs('AGGTAB', 'GXTXAYB')).toBe(4)
-	})
+test('returns the length of the lcs', () => {
+expect(lcs('AGGTAB', 'GXTXAYB')).toBe(4)
+})
 })	
