15dcb1c5d1dd8c6e41ef53074662c5a34215c944
Amrom Steinmetz <amroms@gmail.com>
2019-02-20 09:55:55 -0500

    algorithms: add quickSort

---
+++ algorithms/sorting/src/quickSort.js
@@ -0,0 +1,35 @@
+function quickSort(arr) {
+  function swap(first, second) {
+    const temp = arr[first]
+    arr[first] = arr[second]
+    arr[second] = temp
+  }
+
+  function partition(start, end) {
+    const pivot = arr[end]
+    let pIndex = start
+    for (let i = start; i < end; i++) {
+      if (arr[i] <= pivot) {
+        swap(i, pIndex)
+        pIndex++
+      }
+    }
+
+    swap(end, pIndex)
+    return pIndex
+  }
+
+  function qs(start, end) {
+    if (start >= end) return
+    const p = partition(start, end)
+    qs(start, p - 1)
+    qs(p + 1, end)
+  }
+
+  qs(0, arr.length - 1)
+}
+
+const arr = [10,7,6,8,3,2,4]
+quickSort(arr)
+
+// arr - [ 2, 3, 4, 6, 7, 8, 10 ]
